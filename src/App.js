be8a95c76a013e328a071bdf1fe4e7d8e563a4d9
Les Ansley <les@ansleyfamily.com>
2022-11-24 10:54:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import {CountDisplay, Counter} from "./screens/counter"
+import {CountProvider} from "./context/counter-context"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
