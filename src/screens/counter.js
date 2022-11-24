1ca8a4efe1691e4821188b1630fe543bd8648435
Les Ansley <les@ansleyfamily.com>
2022-11-24 10:52:55 +0200

    Create counter.js

---
+++ src/screens/counter.js
@@ -0,0 +1,14 @@
+import {useCount} from "../context/counter-context"
+
+function CountDisplay() {
+  const [count] = useCount()
+  return <div>{`The current count is ${count}`}</div>
+}
+
+function Counter() {
+  const [, setCount] = useCount()
+  const increment = () => setCount(c => c + 1)
+  return <button onClick={increment}>Increment count</button>
+}
+
+export {CountDisplay, Counter}
